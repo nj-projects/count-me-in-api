7892706409a851a693bb006c5fa33f0e910572d9
Nolan-James-Dev <nj@nolanjames.me>
2024-07-15 17:28:53 +0100

    initial commit

---
+++ src/main/java/me/nolanjames/countmeinapi/CountMeInApiApplication.java
@@ -0,0 +1,13 @@
+package me.nolanjames.countmeinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountMeInApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountMeInApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nolanjames/countmeinapi/CountMeInApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nolanjames.countmeinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountMeInApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
