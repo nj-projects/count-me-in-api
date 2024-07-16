a8112bf18bc43b62e398a315aa253253aa895d44
Nolan-James-Dev <nj@nolanjames.me>
2024-07-16 17:56:21 +0100

    Cors config for UI

---
+++ src/main/java/me/nolanjames/countmeinapi/config/BeanConfig.java
@@ -0,0 +1,36 @@
+package me.nolanjames.countmeinapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.springframework.http.HttpHeaders.*;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                ORIGIN,
+                CONTENT_TYPE,
+                ACCEPT
+        ));
+        configuration.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST"
+        ));
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsFilter(source);
+    }
+}
